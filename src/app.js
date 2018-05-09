c5cd60d657781a3f43a966d9008dacdbfb7f20d8
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2018-05-09 11:33:27 +0800

    Remove dummy data

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux' //provide the store to all of the components
+import { Provider } from 'react-redux' //provide the store to all of the components
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 
@@ -15,21 +15,8 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.subscribe(() => {
-
-  const { expenses, filters } = store.getState();
-  const visibleExpenses = getVisibleExpenses(expenses, filters);
-  console.log(visibleExpenses);
-});
-
-
-
-store.dispatch(addExpense({ description: "Water bill", amount: 4500 }));
-store.dispatch(addExpense({ description: "Gas bill", createdAt: 1000 }));
-store.dispatch(addExpense({ description: "Rent", amount: 109500 }));
-
 const jsx = (
-  <Provider store={store}> 
+  <Provider store={store}>
     <AppRouter />
   </Provider>
 )
